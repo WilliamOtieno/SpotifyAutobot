5c00b0f3dbea013b42f3486b547f5060eee89e7d
WilliamOtieno <jimmywilliamotieno@gmail.com>
2020-04-13 20:36:22 +0300

    Initial commit

---
+++ createplaylist.py
@@ -0,0 +1,30 @@
+#1 Log into YouTube
+#2 Grab our liked videos
+#3 Create a new playlist
+#4 Search for the song
+#5 Add the song into the new spotify playlist
+
+class CreatePlaylist:
+
+    def __init__(self):
+        pass
+    
+    #1 Log into YouTube
+    def get_youtube_client(self):
+        pass
+    
+    #2 Grab our liked videos
+    def get_liked_vides(self):
+        pass
+
+    #3 Create a new playlist
+    def create_playlist(self):
+        pass
+    
+    #4 Search for the song
+    def get_spotify_url(self):
+        pass
+    
+    #5 Add the song into the new spotify playlist
+    def add_song_to_playlist(self):
+        pass
